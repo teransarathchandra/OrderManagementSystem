db8bb0164ac966ea263cd159e57443db4bf85d72
Teran Sarathchandra <tps@tiqri.com>
2025-02-09 10:10:22 +0530

    Refactor: Move OrderItemDto and OrderStatus to new files
    The `OrderItemDto` class, which contains properties `ProductId` and `Quantity`, was originally defined within `CreateOrderDto.cs`. It has now been relocated to its own file `OrderItemDto.cs` and placed under the namespace `Application.Commands.CreateOrder`.
    
    The `OrderStatus` enum, which includes various order statuses such as `Pending`, `Confirmed`, `PaymentFailed`, `Shipped`, `Delivered`, and `Canceled`, was originally defined within `Order.cs`. It has now been moved to its own file `OrderStatus.cs` and placed under the namespace `Domain.Models`.

---
+++ services/order/src/Application/Commands/CreateOrder/OrderItemDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Commands.CreateOrder
+{
+    public class OrderItemDto
+    {
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
