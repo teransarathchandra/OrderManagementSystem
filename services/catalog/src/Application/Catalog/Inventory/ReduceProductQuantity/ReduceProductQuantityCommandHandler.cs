7e4c21cda1ea5e105201529af797f8838dc3a5d5
Teran Sarathchandra <tps@tiqri.com>
2025-02-12 22:09:32 +0530

    Refactor to use primary constructors

--- services/catalog/src/Application/Catalog/Inventory/ReduceProductQuantity/ReduceProductQuantityCommandHandler.cs
+++ services/catalog/src/Application/Catalog/Inventory/ReduceProductQuantity/ReduceProductQuantityCommandHandler.cs
@@ -3,24 +3,18 @@ using MediatR;
 
 namespace Application.Catalog.Inventory.ReduceProductQuantity
 {
-    public class ReduceProductQuantityCommandHandler : IRequestHandler<ReduceProductQuantityCommand, bool>
+    public class ReduceProductQuantityCommandHandler(CatalogDbContext dbContext)
+        : IRequestHandler<ReduceProductQuantityCommand, bool>
     {
-        private readonly CatalogDbContext _dbContext;
-
-        public ReduceProductQuantityCommandHandler(CatalogDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-
         public async Task<bool> Handle(ReduceProductQuantityCommand request, CancellationToken cancellationToken)
         {
-            var product = await _dbContext.Products.FindAsync(request.ProductId);
+            var product = await dbContext.Products.FindAsync(request.ProductId);
 
             if (product == null || product.AvailableQuantity < request.Quantity)
                 return false;
 
             product.AvailableQuantity -= request.Quantity;
-            await _dbContext.SaveChangesAsync(cancellationToken);
+            await dbContext.SaveChangesAsync(cancellationToken);
 
             return true;
         }
