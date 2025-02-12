e49c59221b2b9d3dd87ca6fb1c3b92d6702c16f1
Teran Sarathchandra <tps@tiqri.com>
2025-02-12 15:19:43 +0530

    Refactor: Update project references and Dockerfiles for improved structure and dependency management

--- services/order/src/Infrastructure/Extensions/HttpClientExtensions.cs
+++ services/order/src/Infrastructure/Extensions/HttpClientExtensions.cs
@@ -2,7 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Shared.Extensions
+namespace Infrastructure.Extensions
 {
     public static class HttpClientExtensions
     {
